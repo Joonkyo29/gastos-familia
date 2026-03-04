d6be4785b09fb665c1870fd57f2e5425cd001c62
Joonkyo29 <99111738+Joonkyo29@users.noreply.github.com>
2026-03-04 13:17:52 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,7 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: "AIzaSyAapms6BTNQb8mCjiOanBptBHjgrNZy93c",
   authDomain: "gastos-familia-bde54.firebaseapp.com",
+  databaseURL: "https://gastos-familia-bde54-default-rtdb.firebaseio.com",
   projectId: "gastos-familia-bde54",
   storageBucket: "gastos-familia-bde54.firebasestorage.app",
   messagingSenderId: "704482734980",
